e41f3322a50353b9825cc6d0a25d7c7742fa741d
Brandon Cruz <brandonwilliams1993@yahoo.com>
2024-09-14 14:46:31 -0700

    Animatable

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,22 +1,21 @@
-import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, Modal, StyleSheet, Text, View } from 'react-native';
+import { Input, Rating } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
-import { Rating, Input } from 'react-native-elements';
-import { FontAwesome } from '@expo/vector-icons';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
 import { postComment } from '../features/comments/commentsSlice';
-import { useState } from 'react';
+import * as Animatable from 'react-native-animatable';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
-    const dispatch = useDispatch();
-
     const [showModal, setShowModal] = useState(false);
     const [rating, setRating] = useState(5);
     const [author, setAuthor] = useState('');
     const [text, setText] = useState('');
+    const dispatch = useDispatch();
 
     const handleSubmit = () => {
         const newComment = {
@@ -33,10 +32,6 @@ const CampsiteInfoScreen = ({ route }) => {
         setRating(5);
         setAuthor('');
         setText('');
-        setCampers(1);
-        setHikeIn(false);
-        setDate(new Date());
-        setShowCalendar(false);
     };
 
     const renderCommentItem = ({ item }) => {
@@ -44,9 +39,9 @@ const CampsiteInfoScreen = ({ route }) => {
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
                 <Rating
-                    readonly
                     startingValue={item.rating}
                     imageSize={10}
+                    readonly
                     style={{ alignItems: 'flex-start', paddingVertical: '5%' }}
                 />
                 <Text style={{ fontSize: 12 }}>
@@ -57,7 +52,7 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <>
+        <Animatable.View animation='fadeInUp' duration={2000} delay={1000}>
             <FlatList
                 data={comments.commentsArray.filter(
                     (comment) => comment.campsiteId === campsite.id
@@ -73,7 +68,9 @@ const CampsiteInfoScreen = ({ route }) => {
                         <RenderCampsite
                             campsite={campsite}
                             isFavorite={favorites.includes(campsite.id)}
-                            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                            markFavorite={() =>
+                                dispatch(toggleFavorite(campsite.id))
+                            }
                             onShowModal={() => setShowModal(!showModal)}
                         />
                         <Text style={styles.commentsTitle}>Comments</Text>
@@ -81,7 +78,7 @@ const CampsiteInfoScreen = ({ route }) => {
                 }
             />
             <Modal
-                animationType="slide"
+                animationType='slide'
                 transparent={false}
                 visible={showModal}
                 onRequestClose={() => setShowModal(!showModal)}
@@ -95,19 +92,15 @@ const CampsiteInfoScreen = ({ route }) => {
                         style={{ paddingVertical: 10 }}
                     />
                     <Input
-                        placeholder="Author"
-                        leftIcon={
-                            <FontAwesome name="user-o" size={20} color="black" />
-                        }
+                        placeholder='Author'
+                        leftIcon={{ type: 'font-awesome', name: 'user-o' }}
                         leftIconContainerStyle={{ paddingRight: 10 }}
                         onChangeText={(author) => setAuthor(author)}
                         value={author}
                     />
                     <Input
-                        placeholder="Comment"
-                        leftIcon={
-                            <FontAwesome name="comment-o" size={20} color="black" />
-                        }
+                        placeholder='Comment'
+                        leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
                         leftIconContainerStyle={{ paddingRight: 10 }}
                         onChangeText={(text) => setText(text)}
                         value={text}
@@ -118,8 +111,8 @@ const CampsiteInfoScreen = ({ route }) => {
                                 handleSubmit();
                                 resetForm();
                             }}
-                            color="#5637DD"
-                            title="Submit"
+                            color='#5637DD'
+                            title='Submit'
                         />
                     </View>
                     <View style={{ margin: 10 }}>
@@ -128,13 +121,13 @@ const CampsiteInfoScreen = ({ route }) => {
                                 setShowModal(!showModal);
                                 resetForm();
                             }}
-                            color="#808080"
-                            title="Cancel"
+                            color='#808080'
+                            title='Cancel'
                         />
                     </View>
                 </View>
             </Modal>
-        </>
+        </Animatable.View>
     );
 };
 
@@ -156,17 +149,6 @@ const styles = StyleSheet.create({
     modal: {
         justifyContent: 'center',
         margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        marginBottom: 10,
-        textAlign: 'center'
     }
 });
 
